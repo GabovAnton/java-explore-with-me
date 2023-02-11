51d5ca5ad0455b3ad26532f2e10dc6244622869b
Anton Gabov <antongabov@MacBook-Pro-Anton.local>
2023-02-11 19:05:37 +0300

    fix: add response status to controller in statistic sesrvice

--- stats-service/stat-server/src/main/java/ru/practicum/statistic/StatsController.java
+++ stats-service/stat-server/src/main/java/ru/practicum/statistic/StatsController.java
@@ -2,6 +2,7 @@ package ru.practicum.statistic;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.EndpointHitDto;
@@ -17,6 +18,7 @@ public class StatsController {
     private final StatService statService;
 
     @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<EndpointHitDto> hitWithHttpInfo(@RequestBody EndpointHitDto hitWithHttpInfoDto) {
 
         log.info("Saving hit: {}", hitWithHttpInfoDto);
